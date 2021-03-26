1765b4f7a0b43ed8e4d8880d0bd38b7ddcc82c8c
AGav9020 <44180469+AGav9020@users.noreply.github.com>
2021-03-26 12:07:54 -0700

    Add files via upload

---
+++ MyBot.cs
@@ -0,0 +1,65 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
+using Discord.Net;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBotCntySupport
+{
+    class MyBot
+    {
+        private readonly DiscordSocketClient _client;
+
+        IDiscordClient discord;
+        public MyBot()
+        {
+           // new Program().MainAsync().GetAwaiter().GetResult();
+            _client = new DiscordSocketClient();
+            _client.Log += LogAsync;
+            _client.Ready += ReadyAsync;
+            _client.MessageReceived += MessageReceivedAsync;
+
+          
+        }
+
+        public async Task MainAsync()
+        {
+            // Tokens should be considered secret data, and never hard-coded.
+            await _client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("token"));
+            await _client.StartAsync();
+
+            // Block the program until it is closed.
+            await Task.Delay(-1);
+        }
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log.ToString());
+            return Task.CompletedTask;
+        }
+
+        // The Ready event indicates that the client has opened a
+        // connection and it is now safe to access the cache.
+        private Task ReadyAsync()
+        {
+            Console.WriteLine($"{_client.CurrentUser} is connected!");
+
+            return Task.CompletedTask;
+        }
+        // This is not the recommended way to write a bot - consider
+        // reading over the Commands Framework sample.
+        private async Task MessageReceivedAsync(SocketMessage message)
+        {
+            // The bot should never respond to itself.
+            if (message.Author.Id == _client.CurrentUser.Id)
+                return;
+
+            if (message.Content == "!ping")
+                await message.Channel.SendMessageAsync("pong!");
+        }
+    }
+}
